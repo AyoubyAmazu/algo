9a8703bb1d4f10084f789888e43c61143e5c10d1
AyoubyAmazu <ayoubamazu@gmail.com>
2024-09-10 15:30:46 +0100

    finish algorythm

---
+++ exercise_17.js
@@ -0,0 +1,12 @@
+function fonction17(a, b, c) {
+    let numbers = [a,b,c];
+    // if (a < b && b < c) {
+    //   return [a, b, c];
+    // } else if (b < a && a < c) {
+    //   return [b, a, c];
+    // } else {
+    //   return [c, a, b];
+    // }
+  }
+
+  console.log(fonction17(3,3,5));
