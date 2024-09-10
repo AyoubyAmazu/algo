4d542427e691f8ee2de42457c01b2777097fbfc3
AyoubyAmazu <ayoubamazu@gmail.com>
2024-09-10 09:43:37 +0100

    tri a buller complet

--- ALGO.js
+++ ALGO.js
@@ -7,7 +7,19 @@ for(let i = 0;i<numbers.length;i++){
           temps =  numbers[i]
           numbers[i]=numbers[j]
           numbers[j]=temps  
-          console.log(numbers);
         }
-     }
+      }
+    }
+console.log(numbers);
+
+let newTemps ;
+for(let i = 0 ; i<numbers.length;i++){
+ for(let j =0; j<numbers.length-i-1;j++){
+    if(numbers[j]>numbers[j+1]){
+      newTemps = numbers[j];
+      numbers[j]=numbers[j+1];
+      numbers[j+1] = newTemps;
+    }
+ }
 }
+console.log(numbers);
